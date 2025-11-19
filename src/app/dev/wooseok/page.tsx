82e490bdfd858d7f71782cda1e5343dc7113e5b0
최우석 <nick501@naver.com>
2025-11-20 01:01:11 +0900

    feat: 버튼 사이즈 구분

--- src/app/dev/wooseok/page.tsx
+++ src/app/dev/wooseok/page.tsx
@@ -1,3 +1,22 @@
+import { Button } from '@/components/common/Button';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Button>로그인 하기</Button>
+      <Button variant="outline">로그인 하기</Button>
+      <Button disabled>신청 불가</Button>
+      <Button variant="outline" size="medium">
+        로그인 하기
+      </Button>
+      <Button size="medium">로그인 하기</Button>
+      <Button size="medium" disabled>
+        신청 불가
+      </Button>
+      <Button size="small">로그인 하기</Button>
+      <Button variant="outline" size="small">
+        로그인 하기
+      </Button>
+    </>
+  );
 }
---
+++ src/components/common/Button.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from 'react';
+
+type ButtonSize = 'large' | 'medium' | 'small';
+
+const sizeStyles: Record<ButtonSize, string> = {
+  large: 'px-[136px] py-3.5 tj-body1-bold',
+  medium: 'px-[20px] py-[10px] tj-body2-bold',
+  small: 'px-[12px] py-[8px] tj-caption',
+};
+
+export function Button({
+  type = 'button',
+  variant = 'primary',
+  size = 'large',
+  disabled,
+  children,
+  onClick,
+}: {
+  type?: 'button' | 'submit' | 'reset';
+  variant?: 'primary' | 'outline';
+  size?: ButtonSize;
+  disabled?: boolean;
+  children: ReactNode;
+  onClick?: () => void;
+}) {
+  const primaryStyle = `w-fit bg-[#EA3C12] text-white  rounded-md cursor-pointer`;
+  const disabledStyle = `w-fit bg-gray-40 text-white rounded-md cursor-not-allowed`;
+  const outlineStyle = `w-fit bg-white border border-[#EA3C12] text-[#EA3C12] rounded-md cursor-pointer`;
+
+  const variantStyle = disabled
+    ? disabledStyle
+    : variant === 'outline'
+      ? outlineStyle
+      : primaryStyle;
+  const btnStyle = `${sizeStyles[size]} ${variantStyle}`;
+
+  return (
+    <button type={type} disabled={disabled} className={btnStyle} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
