e864c21f004381837b2419db77e8a2fadede34a1
최우석 <nick501@naver.com>
2025-11-20 19:59:02 +0900

    feat: 이메일 체크 input 제작

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,26 +1,34 @@
-export default function Input({
-  label,
-  value,
-  onChange,
-  error,
-  errorMessage,
-}: {
+interface InputProps {
+  type?: string;
   label: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
   error?: boolean;
   errorMessage?: string;
-}) {
+}
+
+export default function Input({
+  type = 'text',
+  label,
+  value,
+  onChange,
+  onBlur,
+  error,
+  errorMessage,
+}: InputProps) {
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor="input">{label}</label>
       <input
+        type={type}
         value={value}
         placeholder="입력"
-        className={`h-14.5 px-5 py-4 border  rounded-md ${error ? 'border-red-40' : 'border-gray-30'}`}
+        className={`h-14.5 px-5 py-4 bg-white border rounded-md ${error ? 'border-red-40' : 'border-gray-30'}`}
         onChange={onChange}
+        onBlur={onBlur}
       />
-      <p>{error ? errorMessage : ''}</p>
+      <p className="ml-2 text-red-40 text-tj-caption">{error ? errorMessage : ''}</p>
     </div>
   );
 }
--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,11 +1,34 @@
 'use client';
 
+import { useState } from 'react';
 import Input from '../common/Input';
 
-export default function LoginFrom() {
+export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(false);
   return (
     <div>
-      <Input label="아이디" value="" onChange={() => {}} />
+      <Input
+        type="email"
+        label="이메일"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+          setEmailError(false);
+        }}
+        onBlur={(e) =>
+          e.target.value && !isValidEmail(e.target.value)
+            ? setEmailError(true)
+            : setEmailError(false)
+        }
+        error={emailError}
+        errorMessage="잘못된 이메일입니다."
+      />
     </div>
   );
+}
+
+function isValidEmail(email: string) {
+  const EMAIL_PATTERN = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return EMAIL_PATTERN.test(email);
 }
