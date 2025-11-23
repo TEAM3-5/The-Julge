6fbccbd94a96ebd5744b78baa5ee85ba340b6e83
최우석 <nick501@naver.com>
2025-11-24 00:03:36 +0900

    refactor: button 컴포넌트 리팩토링

--- src/app/dev/wooseok/page.tsx
+++ src/app/dev/wooseok/page.tsx
@@ -8,13 +8,17 @@ export default function Home() {
   return (
     <>
       <Button>로그인 하기</Button>
-      <Button variant="outline">로그인 하기</Button>
+      <Button variant="outline" disabled>
+        로그인 하기
+      </Button>
       <Button disabled>신청 불가</Button>
       <Button variant="outline" size="medium">
         로그인 하기
       </Button>
 
-      <Button size="medium">로그인 하기</Button>
+      <Button size="medium" variant="outline" btnColor="blue">
+        로그인 하기
+      </Button>
       <Button size="medium" disabled>
         신청 불가
       </Button>
