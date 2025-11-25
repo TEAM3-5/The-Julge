9c152205ed730a318ad07bb0be9a31a3f66e57a0
최우석 <nick501@naver.com>
2025-11-26 02:01:29 +0900

    fix: label 선택적 렌더링

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,8 +1,8 @@
 import { ComponentPropsWithRef } from 'react';
 
 type InputProps = ComponentPropsWithRef<'input'> & {
-  id: string; // label과 input을 연결하기 위해 필수 prop으로 다시 선언했습니다.
-  label: string;
+  id?: string; // label과 input을 연결할 때 사용하는 id (선택)
+  label?: string; // 라벨이 없는 인풋도 사용할 수 있도록 선택으로 변경
   unit?: string;
   error?: boolean;
   errorMessage?: string;
@@ -18,18 +18,19 @@ export default function Input({
 }: InputProps) {
   return (
     <div className="flex flex-col gap-2">
-      <label htmlFor={id}>{label}</label>
+      {/* label이 전달된 경우에만 렌더링 */}
+      {label && id && <label htmlFor={id}>{label}</label>}
+
       <div
-        className={`flex items-center h-14.5 px-5 py-4 rounded-md bg-white border ${error ? 'border-red-40' : 'border-gray-30'} `}
+        className={`flex items-center h-14.5 px-5 py-4 rounded-md bg-white border ${
+          error ? 'border-red-40' : 'border-gray-30'
+        } `}
       >
-        <input
-          id={id}
-          className={`flex-1 focus:outline-none appearance-none`}
-          {...restInputProps}
-        />
+        <input id={id} className="flex-1 focus:outline-none appearance-none" {...restInputProps} />
         {unit && <span className="text-tj-body1 select-none">{unit}</span>}
       </div>
-      {error && <p className="ml-2 text-red-40 text-tj-caption">{errorMessage}</p>}
+
+      {error && errorMessage && <p className="ml-2 text-red-40 text-tj-caption">{errorMessage}</p>}
     </div>
   );
 }
