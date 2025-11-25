3acfb5251a8964b3083fe9093c021478dddbdf6c
최우석 <nick501@naver.com>
2025-11-26 02:18:56 +0900

    feat: useAuth 도입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import './globals.css';
 import { spoqa } from './fonts';
+import { AuthProvider } from '@/contexts/AuthContext';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" className={spoqa.className}>
-      <body className="min-h-screen bg-white">{children}</body>
+      <body className="min-h-screen bg-white">
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
