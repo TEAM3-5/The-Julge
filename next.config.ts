2137287c9fe4e982f2edd8f36f31d3ba12c19d22
최우석 <nick501@naver.com>
2025-12-01 16:49:52 +0900

    fix: 환경변수 하드코딩

--- next.config.ts
+++ next.config.ts
@@ -9,15 +9,11 @@ const remotePatterns: NonNullable<NextConfig['images']>['remotePatterns'] = [
     protocol: 'https',
     hostname: 'via.placeholder.com',
   },
-];
-
-// 환경 변수가 있을 때만 S3 호스트를 등록한다.
-if (process.env.NEXT_PUBLIC_S3_HOSTNAME) {
-  remotePatterns.push({
+  {
     protocol: 'https',
-    hostname: process.env.NEXT_PUBLIC_S3_HOSTNAME,
-  });
-}
+    hostname: 'bootcamp-project-api.s3.ap-northeast-2.amazonaws.com',
+  },
+];
 
 const nextConfig: NextConfig = {
   images: {
